9db9f9e541ea60362abc351decc71385dd615b2c
Kalyani <115893046+Bogalakalyani@users.noreply.github.com>
2024-11-14 15:15:06 +0530

    Add files via upload

---
+++ single-chat.js
@@ -0,0 +1,38 @@
+const singleChat = [
+    {
+        "senderId": 26172617,     // if senderId is equal to yourID, then you sent this message else you received it
+        "senderName": "Alice",
+        "senderProfilePicture": "http-url",
+        "message": "Hiiii Bob",
+        "type": "text",        // text, image, url
+        "sentAt": "Thu Nov 07 2024 14:56:19 GMT+0530",  // if null, then message not sent
+        "seenAt": "Thu Nov 07 2024 14:56:19 GMT+0530"   // if null, then message not seen yet
+    },
+    {
+        "senderId": 26172646,
+        "senderName": "Bob",
+        "senderProfilePicture": "http-url",
+        "message": "Hello Alice",
+        "type": "text",
+        "sentAt": "Thu Nov 07 2024 14:57:19 GMT+0530",
+        "seenAt": "Thu Nov 07 2024 14:57:19 GMT+0530"
+    },
+    {
+        "senderId": 26172617,
+        "senderName": "Alice",
+        "senderProfilePicture": "http-url",
+        "message": "How are you doing Bob?",
+        "type": "text",
+        "sentAt": "Thu Nov 07 2024 14:58:19 GMT+0530",
+        "seenAt": "Thu Nov 07 2024 14:58:19 GMT+0530" 
+    },
+    {
+        "senderId": 26172646,
+        "senderName": "Bob",
+        "senderProfilePicture": "http-url",
+        "message": "Am doing just fine Alice. Thanks for asking,",
+        "type": "text",
+        "sentAt": "Thu Nov 08 2024 14:59:19 GMT+0530",
+        "seenAt": "Thu Nov 08 2024 14:59:19 GMT+0530"
+    },
+]
